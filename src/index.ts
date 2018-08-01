07fb22e9486726b3cb60a801dc1616fdeae68745
Alex Ford <fordas@uw.edu>
2018-08-01 16:11:23 -0700

    Rebind Esc to move "up" mode insert->normal->command.

--- src/index.ts
+++ src/index.ts
@@ -300,8 +300,8 @@ function activateCellVim(app: JupyterFrontEnd, tracker: INotebookTracker): Promi
             },
             isEnabled
         });
-        commands.addCommand('leave-insert-mode', {
-            label: 'Leave Insert Mode',
+        commands.addCommand('leave-current-mode', {
+            label: 'Leave current mode, moving from insert/visual to normal to command.',
             execute: args => {
                 const current = getCurrent(args);
 
@@ -309,7 +309,13 @@ function activateCellVim(app: JupyterFrontEnd, tracker: INotebookTracker): Promi
                     const { content } = current;
                     if (content.activeCell !== null) {
                         let editor = content.activeCell.editor as CodeMirrorEditor;
-                        (CodeMirror as any).Vim.handleKey(editor.editor, '<Esc>');
+                        if(editor.editor.state.vim.insertMode) {
+                          (CodeMirror as any).Vim.handleKey(editor.editor, '<Esc>');
+                        } else if (editor.editor.state.vim.visualMode){
+                          (CodeMirror as any).Vim.handleKey(editor.editor, '<Esc>');
+                        } else {
+                          commands.execute('notebook:enter-command-mode');
+                        }
                     }
                 }
             },
@@ -472,7 +478,7 @@ function activateCellVim(app: JupyterFrontEnd, tracker: INotebookTracker): Promi
         commands.addKeyBinding({
             selector: '.jp-Notebook.jp-mod-editMode',
             keys: ['Escape'],
-            command: 'leave-insert-mode'
+            command: 'leave-current-mode'
         });
         commands.addKeyBinding({
             selector: '.jp-Notebook:focus',
